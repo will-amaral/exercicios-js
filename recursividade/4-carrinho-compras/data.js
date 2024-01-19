1d6197683ab9b6e84b55c1f194fcb152d9d2a325
Will Amaral <willian.amaral@thoughtworks.com>
2024-01-19 18:42:33 -0300

    fix: update data for recursividade-4

--- recursividade/4-carrinho-compras/data.js
+++ recursividade/4-carrinho-compras/data.js
@@ -1,6 +1,6 @@
 export const produtos = [
   { title: 'Maçã', price: 0.5, unit: 'unidade' },
-  { title: 'Maçã', price: 0.5, unit: 'unidade' },
-  { title: 'Maçã', price: 0.5, unit: 'unidade' },
-  { title: 'Maçã', price: 0.5, unit: 'unidade' },
+  { title: 'Pão', price: 2.2, unit: 'kg' },
+  { title: 'Leite', price: 3.0, unit: 'litro' },
+  { title: 'Frango', price: 12.0, unit: 'kg' },
 ]
