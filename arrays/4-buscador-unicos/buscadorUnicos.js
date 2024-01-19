eee73606a9a015aff2f567a8ac4756f1889f0dea
root <root@DESKTOP-KELP2DU>
2024-01-19 19:17:48 -0300

    adicionar as atividades

--- arrays/4-buscador-unicos/buscadorUnicos.js
+++ arrays/4-buscador-unicos/buscadorUnicos.js
@@ -1,3 +1,13 @@
-export default function getUnique(items) {
-  // Seu código vem aqui
+export default function getUnique(itens) {
+  let numerosUnicos = []
+
+  for (let indice = 0; indice < itens.length; indice++)
+    if (!numerosUnicos.some((numero) => numero === itens[indice])) {
+      numerosUnicos.push(itens[indice])
+    }
+  return numerosUnicos
 }
+
+const items = [1, 2, 2, 3, 4, 4, 5]
+const output = getUnique(items)
+console.log(output)
