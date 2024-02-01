93771df35ac8e1b840ed071eb22f8dfe65f34c08
root <root@DESKTOP-KELP2DU>
2024-02-01 15:36:17 -0300

    fazendo outro push pq achei q tivesse feito o push da pasta inteira mas foi só da atividade 4

--- recursividade/2-string-reversa/stringReversa.js
+++ recursividade/2-string-reversa/stringReversa.js
@@ -1,21 +1,12 @@
 /**
  * @param {string} str - A string a ser revertida
  */
-
-const a = []
 function reverse(str) {
-  // Seu código vem aqui
-  let transformaçao = str.split('')
-  //const a = []
-  if (transformaçao[0] === a[a.length]) {
-    console.log(a.join(''))
+  if (str.length === 0) {
+    return ''
   } else {
-    a.push(transformaçao[transformaçao.length - 1])
-    transformaçao.splice(transformaçao.length - 1, 1)
-    reverse(transformaçao.join(''))
+    return str[str.length - 1] + reverse(str.slice(0, str.length - 1))
   }
 }
-
+//declarar newstring dentro do else e concertar a lógica
 export default reverse
-
-//preciso fazer const a se tornar uma string novamente
--- recursividade/5-palindromo/palindromo.js
+++ recursividade/5-palindromo/palindromo.js
@@ -3,16 +3,15 @@
  */
 function isPalindrome(str) {
   // O seu código vem aqui
-  let palindromo = str.split('')
-  if (palindromo[0] !== palindromo[palindromo.length - 1]) {
+  let newStr = str.replace(/\s/g, '')
+  let strMinisculo = newStr.toLowerCase()
+  if (strMinisculo[0] !== strMinisculo[strMinisculo.length - 1]) {
     return false
-  } else if (palindromo.length === 0) {
+  } else if (strMinisculo.length === 1) {
     return true
   } else {
-    palindromo.splice(0, 1)
-    palindromo.splice(palindromo.length - 1, 1)
-    console.log(palindromo)
-    isPalindrome(palindromo.join(''))
+    newStr = strMinisculo.slice(1, -1)
+    return isPalindrome(newStr)
   }
 }
 
