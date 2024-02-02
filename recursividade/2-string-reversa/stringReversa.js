689cbb16d083180bf047637b8a79776058a5ebaf
Vinícius Vieira Bueno <132572309+ViniciusVBueno@users.noreply.github.com>
2024-02-02 19:17:05 -0300

    Fiz as resposta dos exercicios 1,2 e 3 (#3)

--- recursividade/2-string-reversa/stringReversa.js
+++ recursividade/2-string-reversa/stringReversa.js
@@ -2,7 +2,10 @@
  * @param {string} str - A string a ser revertida
  */
 function reverse(str) {
-  // Seu código vem aqui
+  if (str.length <= 1) {
+    return str
+  } else {
+    return reverse(str.slice(1)) + str[0]
+  }
 }
-
 export default reverse
